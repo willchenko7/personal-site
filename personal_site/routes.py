3bb80fa8d68fb425e6876de8902cfb945a01eebb
Will Khomtchenko <willchenko7@gmail.com>
2023-04-04 23:09:38 -0400

    modified routes and init

--- personal_site/routes.py
+++ personal_site/routes.py
@@ -4,21 +4,17 @@ from personal_site import app
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
-	return render_template('home.html')
+	return render_template('new_home.html')
 
 
 @app.route("/resume", methods=['GET', 'POST'])
 def resume():
-	return render_template('full_resume.html')
+	return render_template('new_resume.html')
 
 @app.route("/connect", methods=['GET', 'POST'])
 def connect():
-	return render_template('connect.html')
+	return render_template('new_connect.html')
 
-@app.route("/apps", methods=['GET', 'POST'])
-def apps():
-	return render_template('apps.html')
-
-@app.route("/interests", methods=['GET', 'POST'])
-def interests():
-	return render_template('interests.html')
+@app.route("/projects", methods=['GET', 'POST'])
+def projects():
+	return render_template('new_projects.html')
